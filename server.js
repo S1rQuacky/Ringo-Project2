4a8fd67f47e1dadbb6e66367f529745b890e9905
Neide Urbina <neide.n.urbina@gmail.com>
2021-10-11 13:37:36 -0500

    Index, New, Create, Show all working

--- server.js
+++ server.js
@@ -67,8 +67,8 @@ app.get('/' , (req, res) => {
 app.get("/spooktober/seed", (req, res) => {
   const mustWatch = [
     {title: "Hocus Pocus", overview: "Max accidentally frees a coven of evil witches, with the help of a magical cat, the kids must steal the witches' book of spells to stop them from becoming immortal.", released:"1993-07-16", myRating: 10, myNotes: "Must watch!", poster: "https://lumiere-a.akamaihd.net/v1/images/p_hocuspocus_19880_e000b013.jpeg?region=0%2C0%2C540%2C810" },
-    {title: "Zombievers", overview:"College friends find their weekend of sex and debauchery ruined when deadly zombie beavers swarm their riverside cabin.", release:"2014-04-13", myRating: 7, myNotes: "Unique idea, perfect ending", poster:"http://gruesome.decadesofhorror.com/wp-content/uploads/sites/6/2015/10/image1.jpeg"},
-    {title: "IT", overview: "In 1960, seven preteen outcasts fight an evil demon that poses as a child-killing clown. Thirty years later, they reunite to stop the demon once and for all when it returns to their hometown.",  release: "1990-11-18", myRating: 8.5, myNotes: "Story is creepy, actor is great, graphics meh, book is amazing", poster:"https://m.media-amazon.com/images/M/MV5BYjg1YTRkNzQtODgyYi00MTQ5LThiMDYtNDJjMWRjNTdjZDZlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg"}
+    {title: "Zombievers", overview:"College friends find their weekend of sex and debauchery ruined when deadly zombie beavers swarm their riverside cabin.", released:"2014-04-13", myRating: 7, myNotes: "Unique idea, perfect ending", poster:"http://gruesome.decadesofhorror.com/wp-content/uploads/sites/6/2015/10/image1.jpeg"},
+    {title: "IT", overview: "In 1960, seven preteen outcasts fight an evil demon that poses as a child-killing clown. Thirty years later, they reunite to stop the demon once and for all when it returns to their hometown.",  released: "1990-11-18", myRating: 8.5, myNotes: "Story is creepy, actor is great, graphics meh, book is amazing", poster:"https://m.media-amazon.com/images/M/MV5BYjg1YTRkNzQtODgyYi00MTQ5LThiMDYtNDJjMWRjNTdjZDZlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg"}
   ]
   MovieModel.remove({}, (err, data) => {
     MovieModel.create(mustWatch, (err, data) => {
@@ -77,9 +77,30 @@ app.get("/spooktober/seed", (req, res) => {
   });
 });
 
+//Index
 app.get("/spooktober", (req, res) => {
   MovieModel.find({}, (err, flicks) => {
     res.render("spooktober/index.ejs", {flicks});
+  });
+});
+
+//New
+app.get("/spooktober/new", (req, res) => {
+  res.render("spooktober/new.ejs")
+});
+
+//Create
+app.post("/spooktober", (req, res) => {
+  MovieModel.create(req.body, (err, movie) => {
+    res.redirect("/spooktober")
+  });
+});
+
+//Show
+app.get("/spooktober/:id", (req, res) => {
+  const id = req.params.id
+  MovieModel.findById(id, (err, movie) => {
+    res.render("spooktober/show.ejs", {movie})
   });
 });
 
